ff2d226e38a28421c24d0856b3c941dc1cdd62a8
arppzi666-hub <arppzi666@gmail.com>
2026-06-17 11:01:29 +0800

    Add ping endpoint to verify deployment

---
+++ api/ping.js
@@ -0,0 +1,4 @@
+﻿module.exports = function(req, res) {
+  res.writeHead(200, {"Content-Type":"application/json; charset=utf-8","Access-Control-Allow-Origin":"*"});
+  res.end(JSON.stringify({version:"v3-NEW-CODE-2026",time:new Date().toISOString()}));
+};
